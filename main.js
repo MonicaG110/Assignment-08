06167a0b005f5e30b877e1ebfe199b74f4838c47
Monica <monicamillerg@gmail.com>
2015-10-17 12:23:55 -0700

    answer 2 complete

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
 var matchingPriceItems;
 $(document).ready(function(){
 
-	function answer1() {
-		var totalPrice = items.reduce(function(a,b){
 
-			if (typeof a === "number"){
+	// ANSWER1
+	var totalPrice = items.reduce(function(a,b){
 
-				return a + b.price;
-			
-			} else {
+		if (typeof a === "number"){
 
-				return a.price + b.price;
-			}
+			return a + b.price;
+		
+		} else {
 
-		});
+			return a.price + b.price;
+		}
 
-		var avgPrice = totalPrice / items.length;
-		var answer1Html = "<p>The average price is $" + avgPrice.toFixed(2) + "</p>";
-		$("#answer1").html(answer1Html);
-	}
+	});
 
-	answer1();
-	/*
+	var avgPrice = totalPrice / items.length;
+	var answer1Html = "<p>The average price is $" + avgPrice.toFixed(2) + "</p>";
+	$("#answer1").html(answer1Html);
+
+
+	// ANSWER2
 	matchingPriceItems = items.filter(function(item){
 		var minPrice = 14;
 		var maxPrice = 18;
-		if (item.price >= minPrice && item.price <= maxPrice ) {	 
+		if (item.price > minPrice && item.price < maxPrice ) {	 
 			return item;
 		}
 	});
-	$("#answer2").html(matchingPriceItems.map(function(item){
-		return item.title;
-	}));
 
-	/*	
-	var titles = matchingPriceItems.map(function(item){
+	var titleHtml = matchingPriceItems.map(function(item){
 		return "<p>" + item.title + "</p>";
 	});
-	*/
-	
+
+	$("#answer2").html(titleHtml);
 
 });
 
